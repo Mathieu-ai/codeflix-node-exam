5261c111e68ba0091b8c1912b9d690a1d104ff9c
Mathieu Lièvre <mathieu.lievre@efrei.net>
2020-11-18 15:59:59 +0100

    adding ex3

--- e03/mycp.js
+++ e03/mycp.js
@@ -4,11 +4,10 @@ if (process.argv.length==4) {
   fs.copyFile(process.argv[2], process.argv[3], (err) => {
     if (err) throw err;
     console.log('File was copied to destination');
-  }); /*
-} else if (process.argv.length==5 && process.argv[2]==='-r' ) {
-  fs.writeFile(process.argv[3], 'if you read this, then the file is succefully created', function (err) {
-    if (err) throw err;
-    console.log('File is created successfully.');
-  });
+  }); 
+} else if (process.argv.length==4 && process.argv[2]==='-r' ) {
+  fs.copyFile( process.argv[3], process.argv[4], fs.constants.COPYFILE_EXC,function (err, data) {
+      if (err) throw err;
+      console.log(data)
+  }
 }
-*/
